a9cca7287e972d1696ba28f8a7f03489b2c6bc08
Pranav Gupta <pranavgupta000@gmail.com>
2019-10-31 18:56:06 +0530

    linked heirarchy visualisation

--- relationHeirarchyDjango/core/urls.py
+++ relationHeirarchyDjango/core/urls.py
@@ -7,5 +7,6 @@ app_name = 'core'
 urlpatterns = [
     path('', TemplateView.as_view(template_name="core/home.html", extra_context={'activate':'home'}), name="home"),
     path('download', TemplateView.as_view(template_name="core/download.html", extra_context={'activate':'downloads'}), name="download"),
+    path('heirarchy', TemplateView.as_view(template_name="core/jsontree.html", extra_context={'activate':'heirarchy'}), name="heirarchy"),
       
 ]
