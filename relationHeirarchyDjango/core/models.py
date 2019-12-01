ca788c3180d4bfa490e8381490eec3be2ccca38f
Pranav <pranavgupta000@gmail.com>
2019-12-01 11:20:08 +0530

    heirarchy works

--- relationHeirarchyDjango/core/models.py
+++ relationHeirarchyDjango/core/models.py
@@ -9,3 +9,18 @@ class Tuple(models.Model):
 
 	def __str__(self):
 		return f'{self.parent}-{self.child}'
+
+
+# class TupleCopy(models.Model):
+# 	id = models.IntegerField(validators=[MinValueValidator(0)], primary_key=True)
+# 	parent = models.CharField(max_length=100)
+# 	child = models.CharField(max_length=100)
+# 	sump = models.IntegerField(default=0,validators=[MinValueValidator(0)])
+# 	# neg = models.IntegerField(default=0,validators=[MinValueValidator(0)])
+
+# 	def __str__(self):
+# 		return f'{self.parent}-{self.child}'	
+
+# class RangeIndex(models.Model):
+# 	startIndex = models.IntegerField(default=0,validators=[MinValueValidator(0)])
+# 	# endIndex = models.IntegerField(default=0, validators=[MinValueValidator(0)])
